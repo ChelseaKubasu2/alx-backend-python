e6d92bacdcb86f88ba5f4c3103865d0c54868468
ChelseaKubasu2 <kubasuchelsea@gmail.com>
2023-01-06 02:03:17 +0000

    add files

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+'''
+    Defines function floor
+'''
+
+
+def floor(n: float) -> int:
+    ''' Round float down to nearest whole number '''
+    import math
+    return math.floor(n)
